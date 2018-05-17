e5fa639adaf0af9839ad4c183fb52f9f71dae329
Vaztonyo <tony.dewet7@gmail.com>
2018-05-17 16:40:15 +0200

    navbar fixed

--- processes.js
+++ processes.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
         $('#fullpage').fullpage({
             anchors: ['analysis', 'research', 'wireframes', 'design', 'development', 'test']
         });
-            var request = $.get("https://design-sprint-garage.prismic.io/api/v2/documents/search?ref=Wv1H8SgAAKZKkGqI&q=[[at(document.type,%22garage-process%22)]]", result => result)
+       
+            var request = $.get("https://design-sprint-garage.prismic.io/api/v2/documents/search?ref=Wv2A0ygAANFMkWXi&q=[[at(document.type,%22garage-process%22)]]", result => result)
                     .done(function (json) {
                             const results = JSON.parse(json).results
                             console.log("RESULTS", results);
@@ -16,8 +17,8 @@ $(document).ready(function () {
                             $(".content-research-img").attr("src", researchImage);
     
                             var wireframeImage = results[0].data.process_image.url;
-                            var imgElem3 = document.querySelector(".content-wireframe-img");
-                            $(".content-wireframe-img").attr("src", wireframeImage);
+                            var imgElem3 = document.querySelector(".content-wireframes-img");
+                            $(".content-wireframes-img").attr("src", wireframeImage);
                             
                             var designImage = results[3].data.process_image.url;
                             var imgElem4 = document.querySelector(".content-design-img");
@@ -30,6 +31,54 @@ $(document).ready(function () {
                             var testingImage = results[2].data.process_image.url;
                             var imgElem6 = document.querySelector(".content-testing-img");
                             $(".content-testing-img").attr("src", testingImage);
+
+                            var analysisHead = results[5].data.process_type[0].text;
+                            var analysisHeadElem = document.querySelector(".analysis-header");
+                            analysisHeadElem.innerHTML = analysisHead;
+    
+                            var researchHead = results[4].data.process_type[0].text;
+                            var researchHeadElem = document.querySelector(".research-header");
+                            researchHeadElem.innerHTML = researchHead;
+
+                            var wireframesHead = results[0].data.process_type[0].text;
+                            var wireframesHeadElem = document.querySelector(".wireframes-header");
+                            wireframesHeadElem.innerHTML = wireframesHead;
+    
+                            var designHead = results[3].data.process_type[0].text;
+                            var designHeadElem = document.querySelector(".design-header");
+                            designHeadElem.innerHTML = designHead;
+    
+                            var developHead = results[1].data.process_type[0].text;
+                            var developHeadElem = document.querySelector(".develop-header");
+                            developHeadElem.innerHTML = developHead;
+    
+                            var testHead = results[2].data.process_type[0].text;
+                            var testHeadElem = document.querySelector(".test-header");
+                            testHeadElem.innerHTML = testHead;
+
+                            var analysistext = results[5].data.process_details[0].text;
+                            var analysistextElem = document.querySelector(".analysis-text");
+                            analysistextElem.innerHTML = analysistext;
+    
+                            var researchtext = results[4].data.process_details[0].text;
+                            var researchtextElem = document.querySelector(".research-text");
+                            researchtextElem.innerHTML = researchtext;
+
+                            var wireframestext = results[0].data.process_details[0].text;
+                            var wireframestextElem = document.querySelector(".wireframes-text");
+                            wireframestextElem.innerHTML = wireframestext;
+    
+                            var design = results[3].data.process_details[0].text;
+                            var designElem = document.querySelector(".design-text");
+                            designElem.innerHTML = design;
+    
+                            var developtext = results[1].data.process_details[0].text;
+                            var developtextElem = document.querySelector(".develop-text");
+                            developtextElem.innerHTML = developtext;
+    
+                            var testtext = results[2 ].data.process_details[0].text;
+                            var testtextElem = document.querySelector(".test-text");
+                            testtextElem.innerHTML = testtext;
                             
                             
                     })
