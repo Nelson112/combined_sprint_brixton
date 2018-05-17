1b88b9b828eaec59bcac29f0f8be523cb64b5c7f
Nelson Mgengwana <nelsonmgengwana@gmail.com>
2018-05-17 14:33:22 +0200

    modal added

--- applications.js
+++ applications.js
@@ -1,10 +1,23 @@
 $(document).ready(function() {
 
 $('.collapsible').collapsible();
+$('.modal').modal();
 
-$( "#non-inno-people" ).click(function() {
-    document.getElementById('new-peeps').style.display = "block";
-    document.getElementById("startPage").style.display = "none";
-
-  });
 });
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
